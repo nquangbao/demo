a40628ef225e93b2ce863a571f11a781217e65c1
otisroot <58733000+otisroot@users.noreply.github.com>
2020-02-27 14:59:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+new WOW().init();
 setTimeout(function(){ 
 	document.querySelector('div#exampleModalCenter').style.display = "block";
 	document.querySelector('div#exampleModalCenter').style.opacity  = "1";
-}, 3000);
-new WOW().init();
+}, 2000);
 document.querySelector('button#close').onclick = () => {
 	document.querySelector('div#exampleModalCenter').style.display = "none";
 	document.querySelector('div#exampleModalCenter').style.opacity  = "0";
